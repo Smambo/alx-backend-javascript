6cd399baff2e0df03fd4cf4aa4eb5e33e9f62449
Simamnkele <smambo85@gmail.com>
2024-03-05 17:54:49 +0000

    Fixed typo

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-        stattus: 200,
+        status: 200,
         body: 'Success',
       });
     } else {
