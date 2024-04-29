6a23e4723f64efb6305cf888ae96b7bb7bfd9859
Simamnkele <smambo85@gmail.com>
2024-04-29 18:18:47 +0200

    Pushed process.stdin program

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// program executed through command line
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
